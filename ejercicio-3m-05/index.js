58ec9338c4b979a9211e82a6e74527402d911b46
jsharo <jsharo@sudamericano.edu.ec>
2025-01-14 09:34:22 -0500

    Add files via upload

---
+++ ejercicio-3m-05/index.js
@@ -0,0 +1,7 @@
+let nums = [12, 85, 10, 36];
+let contador = 0;
+
+for (let i = 0; i < nums.length; i++) {
+    contador ++;
+}
+alert("longitud " + contador);
---
+++ ejercicio-3m-06/index.js
@@ -0,0 +1,10 @@
+let nums = [1,2,3,4,5];
+let contador = 0;
+
+for(let i = 0; i < nums.length; i++) {
+    if(nums[i] % 2 !== 1) {
+        contador ++;
+}
+}
+alert(contador);
+
